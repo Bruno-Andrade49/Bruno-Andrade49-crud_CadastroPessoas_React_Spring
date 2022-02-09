ff347d577a91a135d8969ed40f6d9b495ea4285e
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-02-09 18:31:31 -0300

    .

---
+++ Back-End/src/test/java/com/crudreact/BackEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudreact.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
