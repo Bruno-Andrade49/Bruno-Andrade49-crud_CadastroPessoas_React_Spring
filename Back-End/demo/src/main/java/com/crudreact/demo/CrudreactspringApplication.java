13356e03a4a9836e47c4c4038a69faf980a6a132
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-02-09 17:28:54 -0300

    First commit

---
+++ Back-End/demo/src/main/java/com/crudreact/demo/CrudreactspringApplication.java
@@ -0,0 +1,13 @@
+package com.crudreact.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudreactspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudreactspringApplication.class, args);
+	}
+
+}
---
+++ Back-End/demo/src/test/java/com/crudreact/demo/CrudreactspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudreact.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudreactspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
