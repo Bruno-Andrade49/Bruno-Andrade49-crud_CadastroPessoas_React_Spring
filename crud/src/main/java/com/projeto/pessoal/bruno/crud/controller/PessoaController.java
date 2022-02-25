d06a34f29ed1ad5f74ec1765cde1475da5fa0966
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-02-25 10:37:50 -0300

    .

--- crud/src/main/java/com/projeto/pessoal/bruno/crud/controller/PessoaController.java
+++ crud/src/main/java/com/projeto/pessoal/bruno/crud/controller/PessoaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,7 +44,7 @@ public class PessoaController {
 
 		
 		//DELETE
-		@CrossOrigin("http://localhost:3000")
+		@CrossOrigin(methods = RequestMethod.DELETE, origins = "http://localhost:3000/")
 		@PutMapping(path = "/cadastro/{idPessoa}")
 		@ResponseStatus(value = HttpStatus.OK)
 		public void deletarPessoa(@PathVariable String idPessoa) {
