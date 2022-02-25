55cb8090df5ff5f8bfff4e4ced68b5965318de66
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-02-25 11:08:53 -0300

    .

--- crud/src/main/java/com/projeto/pessoal/bruno/crud/controller/PessoaController.java
+++ crud/src/main/java/com/projeto/pessoal/bruno/crud/controller/PessoaController.java
@@ -29,7 +29,6 @@ public class PessoaController {
 
 		
 		//GET
-		@CrossOrigin(methods = RequestMethod.GET, origins = "http://localhost:3000")
 		@GetMapping(path = "/pessoas")
 		@ResponseStatus(value = HttpStatus.FOUND)
 		public List<Pessoa> getAllPessoas() {
@@ -38,7 +37,6 @@ public class PessoaController {
 
 		
 		//POST
-		@CrossOrigin(methods = RequestMethod.POST, origins = "http://localhost:3000")
 		@PostMapping(path = "/cadastro")
 		@ResponseStatus(value = HttpStatus.CREATED)
 		public Pessoa criarPessoa(@RequestBody Pessoa pessoa) {
@@ -47,7 +45,6 @@ public class PessoaController {
 
 		
 		//DELETE
-		@CrossOrigin(methods = RequestMethod.DELETE, origins = "http://localhost:3000")
 		@PutMapping(path = "/cadastro/{idPessoa}")
 		@ResponseStatus(value = HttpStatus.OK)
 		public void deletarPessoa(@PathVariable String idPessoa) {
@@ -57,7 +54,6 @@ public class PessoaController {
 
 		
 		//EDIT
-		@CrossOrigin(methods = RequestMethod.PUT, origins = "http://localhost:3000")
 		@PutMapping(path = "/cadastro/{idPessoa}/edit")
 		@ResponseStatus(value = HttpStatus.OK)
 		public void editarPessoa(@PathVariable String idPessoa, @RequestBody Pessoa pessoa) {
