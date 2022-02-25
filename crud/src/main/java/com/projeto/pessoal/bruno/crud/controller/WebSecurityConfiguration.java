5b2d85502aa8c0812e0f459ffb31aad6177d39e3
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-02-25 15:43:02 -0300

    .

---
+++ crud/src/main/java/com/projeto/pessoal/bruno/crud/controller/WebSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.projeto.pessoal.bruno.crud.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("").password("").roles("User");
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+    }
+}
