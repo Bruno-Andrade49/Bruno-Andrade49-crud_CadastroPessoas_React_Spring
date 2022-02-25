a127b2b027b30a9688d43ece2e737009882324e9
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-02-25 11:43:36 -0300

    .

---
+++ crud/src/main/java/com/projeto/pessoal/bruno/crud/controller/WebConfig.java
@@ -0,0 +1,22 @@
+package com.projeto.pessoal.bruno.crud.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/cadastro/**")
+			.allowedOrigins("http://localhost:3000")
+			.allowedMethods("PUT", "DELETE")
+			.allowedHeaders("header1", "header2", "header3")
+			.exposedHeaders("header1", "header2")
+			.allowCredentials(true)
+			.maxAge(3600);
+	}
+}
