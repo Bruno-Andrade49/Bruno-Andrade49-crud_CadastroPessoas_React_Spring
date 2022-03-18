4bc91876abfd114a93d875fc0077475b9efca594
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-03-18 17:15:50 -0300

    +

--- crud/src/main/java/com/projeto/api/bruno/crud/Crud/controller/PessoaController.java
+++ crud/src/main/java/com/projeto/api/bruno/crud/Crud/controller/PessoaController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.projeto.api.bruno.crud.Crud.repositories.PessoaRepository;
 
 @RestController
 @RequestMapping(path = "/pessoas")
+@CrossOrigin(originPatterns = "http://localhost:3000")
 public class PessoaController {
 	
 	@Autowired
